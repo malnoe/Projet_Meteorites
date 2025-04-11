ab5a6c876a9dfb93ae01b24a2197bb835a0f90fc
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2025-04-11 02:17:53 +0200

    Math processus de poisson début et changement images rapport en R

--- Exploration des données/script_analyses_multivariées.R
+++ Exploration des données/script_analyses_multivariées.R
@@ -1,7 +1,103 @@
+# Packages
 library(corrr)
 library(xtable)
+library(ggplot2)
+library(dplyr)
 
 Meteorite_Landings <- read.csv("~/Ecole/M1/Projet/Projet_Meteorites/Données/Meteorite_Landings.csv")
+
+# Analyses univariées
+
+# Name - Première lettre
+Meteorite_Landings <- Meteorite_Landings %>%
+  mutate(first_letter = substr(name, 1, 1))
+# Compter le nombre d'occurrences de chaque première lettre et trier par ordre décroissant
+letter_counts <- Meteorite_Landings %>%
+  group_by(first_letter) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))  # Trier par ordre décroissant
+# Créer l'histogramme
+ggplot(letter_counts, aes(x = reorder(first_letter, -count), y = count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Histogramme des premières lettres des noms de météorites",
+       x = "Première lettre",
+       y = "Nombre de météorites") +
+  theme_minimal()
+
+#Recclass
+# Calcul pourcentage en ne gardant que ceux > 4%
+recclass_counts <- Meteorite_Landings %>%
+  group_by(recclass) %>%
+  summarise(count = n()) %>%
+  mutate(percentage = count / sum(count) * 100)
+# Regrouper les classes qui représentent moins de 4 % dans "Autres"
+recclass_counts <- recclass_counts %>%
+  mutate(recclass = ifelse(percentage < 4, "Autres", recclass)) %>%
+  group_by(recclass) %>%
+  summarise(count = sum(count), percentage = sum(percentage))
+# Trier par pourcentage
+recclass_counts <- recclass_counts %>%
+  arrange(percentage)
+# Pie chart
+ggplot(recclass_counts, aes(x = "", y = percentage, fill = recclass)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y") +
+  labs(title = "Répartition des classes de recclass",
+       fill = "Classe") +
+  theme_void() +  # Utiliser un thème vide pour un pie chart
+  geom_text(aes(label = paste0(round(percentage, 1), "%")), 
+            position = position_stack(vjust = 0.5),
+            size=3,
+            color="white")
+
+# Year
+# Filtrer les données pour ne garder que les années > 1963 et < 2026
+filtered_data <- Meteorite_Landings %>%
+  filter(year > 1963 & year <2026)
+# Compter le nombre d'occurrences de chaque année
+year_counts <- filtered_data %>%
+  group_by(year) %>%
+  summarise(count = n())
+# Créer le barplot
+ggplot(year_counts, aes(x = year, y = count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Nombre de météorites tombées chaque année (après 1963)",
+       x = "Année",
+       y = "Nombre de météorites") +
+  theme_minimal()
+
+# Latitude
+# Arrondir les valeurs de reclat au degré le plus proche et compter les occurrences
+latitude_counts <- Meteorite_Landings %>%
+  mutate(rounded_lat = round(reclat)) %>%  # Arrondir au degré le plus proche
+  group_by(rounded_lat) %>%
+  summarise(count = n())
+# Barplot
+ggplot(latitude_counts, aes(x = rounded_lat, y = count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Météorites tombées par degré de latitude",
+       x = "Latitude (degré arrondi à l'unité)",
+       y = "Nombre de météorites") +
+  theme_minimal()
+
+# Longitude
+# Arrondir les valeurs de reclat au degré le plus proche et compter les occurrences
+longitude_counts <- Meteorite_Landings %>%
+  filter(reclong >= -180 & reclong <= 180)
+longitude_counts <- longitude_counts %>%
+  mutate(rounded_long = round(reclong)) %>%  # Arrondir au degré le plus proche
+  group_by(rounded_long) %>%
+  summarise(count = n())
+# Barplot
+ggplot(longitude_counts, aes(x = rounded_long, y = count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Météorites tombées par degré de longitude",
+       x = "Longitude (degré arrondi à l'unité)",
+       y = "Nombre de météorites") +
+  theme_minimal()
+
+
+# Analyses multivariées
 Meteorite_Landings <- na.omit(Meteorite_Landings)
 col_quanti <- c(5,7,8,9)
 col_quali <- c(3,4,6)
